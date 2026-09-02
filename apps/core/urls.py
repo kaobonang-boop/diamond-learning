4e8c30d8b8756bac8067f362a591e238c8513feb
kaobonang-boop <kaobonang@gmail.com>
2026-09-02 11:32:16 +0200

    urls.py

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("dashboard/", views.dashboard, name="dashboard"),
     path("search/", views.search, name="search"),
+    path("search/suggest/", views.search_suggest, name="search_suggest"),
 ]
